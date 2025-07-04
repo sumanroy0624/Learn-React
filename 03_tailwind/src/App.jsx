25a4edc648135b8de2f9caa2daff637b125f1a7a
Suman <suman@gmail.com>
2025-07-05 03:02:54 +0530

    This commit is massive

--- 03_tailwind/src/App.jsx
+++ 03_tailwind/src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Drop_down from "../components/drop_down";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <>
+    <Drop_down Name="suman roy" myob={{name:'suman', year:'third'}} myArr={[1,2,4,5]} />
+    <br />
+    <Drop_down Name="Sumana Roy" myob={{name:'Sumana', year:'1st'}} myArr={[1,2,4,5]}/>
+   </>
+  
+  );
 }
-
-export default App
