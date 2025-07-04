0f28b39be3d4886d0208ebf0eb232bec97aa1510
Suman <suman@gmail.com>
2025-07-04 20:06:36 +0530

    01_basic React Commited

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Sample from './sample';
+const userName="Suman Roy";
+
+const ReactApp=React.createElement(
+  'a',
+  {href:"https://google.com", target:'_blank'},
+  'Click Me !',
+  userName,
+  
+  
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+     ReactApp
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/sample.js
@@ -0,0 +1,8 @@
+function Sample(){
+  return (
+
+   <h2>This is from sample</h2>
+
+  )
+}
+export default Sample;
